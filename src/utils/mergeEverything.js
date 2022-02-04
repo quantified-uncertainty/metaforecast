a7c09e14ae51eb631deff912dfbf49db1ee0e5ea
NunoSempere <nuno.sempere@protonmail.com>
2022-02-04 10:42:03 -0500

    chore: Cleanup.
    - Deleted foretold community on astralcodexten questions
      - because now they are managed by Metaculus/Manifold
    - fixed small mistake for good judgment
    - recalculated scores for manifold markets
      - avoid low-quality markets.

--- src/utils/mergeEverything.js
+++ src/utils/mergeEverything.js
@@ -1,23 +1,53 @@
-import { mongoRead, upsert } from "./mongo-wrapper.js"
+import { mongoRead, upsert } from "./mongo-wrapper.js";
 
 /* Merge everything */
-let sets = ["astralcodexten", "betfair", "coupcast", "csetforetell", "elicit", /* "estimize" ,*/ "fantasyscotus", "foretold", "givewellopenphil", "goodjudgment","goodjudmentopen", "hypermind", "kalshi", "ladbrokes", "manifoldmarkets", "metaculus", "omen", "polymarket", "predictit", "rootclaim", "smarkets", "wildeford", "williamhill", "xrisk"]
-let suffix = "-questions"
+// let sets = ["astralcodexten", "betfair", "coupcast", "csetforetell", "elicit", /* "estimize" ,*/ "fantasyscotus", "foretold", "givewellopenphil", "goodjudgment","goodjudmentopen", "hypermind", "kalshi", "ladbrokes", "manifoldmarkets", "metaculus", "omen", "polymarket", "predictit", "rootclaim", "smarkets", "wildeford", "williamhill", "xrisk"]
+let sets = [
+  "betfair",
+  "coupcast",
+  "csetforetell",
+  "elicit",
+  /* "estimize" ,*/ "fantasyscotus",
+  "foretold",
+  "givewellopenphil",
+  "goodjudgment",
+  "goodjudmentopen",
+  "hypermind",
+  "kalshi",
+  "ladbrokes",
+  "manifoldmarkets",
+  "metaculus",
+  "omen",
+  "polymarket",
+  "predictit",
+  "rootclaim",
+  "smarkets",
+  "wildeford",
+  "williamhill",
+  "xrisk",
+];
 
-export async function mergeEverythingInner(){
-  let merged = []
-  for(let set of sets){
-    let json = await mongoRead(set+suffix)
-    console.log(`${set} has ${json.length} questions\n`)
-    merged = merged.concat(json)
+let suffix = "-questions";
+
+export async function mergeEverythingInner() {
+  let merged = [];
+  for (let set of sets) {
+    let json = await mongoRead(set + suffix);
+    console.log(`${set} has ${json.length} questions\n`);
+    merged = merged.concat(json);
   }
-  let mergedprocessed = merged.map(element => ({...element, optionsstringforsearch: element.options.map(option => option.name).join(", ")}))
-  console.log(`In total, there are ${mergedprocessed.length} questions`)
-  return mergedprocessed
+  let mergedprocessed = merged.map((element) => ({
+    ...element,
+    optionsstringforsearch: element.options
+      .map((option) => option.name)
+      .join(", "),
+  }));
+  console.log(`In total, there are ${mergedprocessed.length} questions`);
+  return mergedprocessed;
 }
 
-export async function mergeEverything(){
-  let merged = await mergeEverythingInner()
-  await upsert( merged,"metaforecasts")
-  console.log("Done")
+export async function mergeEverything() {
+  let merged = await mergeEverythingInner();
+  await upsert(merged, "metaforecasts");
+  console.log("Done");
 }
