d921885f1bb24376a93c59c56f6fde4ab1344e10
Vyacheslav Matyukhin <me@berekuk.ru>
2022-03-26 14:23:38 +0300

    feat: nprogress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,12 @@
+import 'nprogress/nprogress.css';
 import '../styles/main.css';
+
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
