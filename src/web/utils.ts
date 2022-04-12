846e235e0c28b0cbffeaa37d9beb3ac998f57e5e
Vyacheslav Matyukhin <me@berekuk.ru>
2022-04-12 11:37:17 +0300

    fix: NEXT_PUBLIC_VERCEL_URL usage

--- src/web/utils.ts
+++ src/web/utils.ts
@@ -2,7 +2,7 @@ import { IncomingMessage } from "http";
 
 export const reqToBasePath = (req: IncomingMessage) => {
   if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    return process.env.NEXT_PUBLIC_VERCEL_URL;
   }
 
   // we could just hardcode http://localhost:3000 here, but then `next dev -p <CUSTOM_PORT>` would break
