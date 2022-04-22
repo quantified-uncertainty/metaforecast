66938bcaf41d3cb9f4ba9ab61c4152f90b085323
Vyacheslav Matyukhin <me@berekuk.ru>
2022-04-23 00:40:24 +0400

    debug: exploratory logging for frontpage

--- src/graphql/schema/frontpage.ts
+++ src/graphql/schema/frontpage.ts
@@ -7,7 +7,12 @@ builder.queryField("frontpage", (t) =>
     type: [QuestionObj],
     description: "Get a list of questions that are currently on the frontpage",
     resolve: async () => {
-      return await getFrontpage();
+      try {
+        return await getFrontpage();
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
     },
   })
 );
