396a39372d8ca6adffd290be149b3088edae1efc
Vyacheslav Matyukhin <me@berekuk.ru>
2022-04-27 22:27:36 +0400

    fix: pointer cursor for CopyText

--- src/web/common/CopyText.tsx
+++ src/web/common/CopyText.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const CopyText: React.FC<Props> = ({ text, displayText }) => (
   <div
-    className="flex items-center justify-center p-4 space-x-3 border rounded border-blue-400 hover:border-transparent bg-transparent hover:bg-blue-300 text-sm font-medium text-blue-400 hover:text-white"
+    className="flex items-center justify-center p-4 space-x-3 border rounded border-blue-400 hover:border-transparent bg-transparent hover:bg-blue-300 text-sm font-medium text-blue-400 hover:text-white cursor-pointer"
     onClick={(e) => {
       e.preventDefault();
       navigator.clipboard.writeText(text);
