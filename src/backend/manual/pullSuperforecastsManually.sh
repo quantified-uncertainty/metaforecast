c9bfba84740fc42982e8d96f8e6b1e4e16458903
NunoSempere <nuno.sempere@protonmail.com>
2022-07-08 17:49:27 -0400

    fix: Good Judgment workflow

--- src/backend/manual/pullSuperforecastsManually.sh
+++ src/backend/manual/pullSuperforecastsManually.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /home/loki/Documents/core/software/fresh/js/metaforecast/metaforecast-monorepo
-/home/loki/.nvm/versions/node/v17.5.0/bin/npm run cli 3 > ../last-superforecast-fetch.txt
+
+/home/loki/.nvm/versions/node/v16.15.0/lib/node_modules/npm/bin/npm-cli.js run cli goodjudgment  > ../last-superforecast-fetch.txt
