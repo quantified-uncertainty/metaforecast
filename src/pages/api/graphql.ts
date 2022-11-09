38a2fe8215bc3f24615150fcb9ad28df7b3c2403
NunoSempere <nuno.sempere@protonmail.com>
2022-11-09 21:54:08 +0000

    feat: add caching!!

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,13 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import {NextApiRequest, NextApiResponse} from "next";
 
 // apollo-server-micro is problematic since v3, see https://github.com/apollographql/apollo-server/issues/5547, so we use graphql-yoga instead
-import { createYoga } from "graphql-yoga";
+import {createYoga} from "graphql-yoga";
+import {useResponseCache} from '@graphql-yoga/plugin-response-cache'
 
-import { schema } from "../../graphql/schema";
+import {schema} from "../../graphql/schema";
 
-const server = createYoga<{
+const server = createYoga < {
   req: NextApiRequest;
   res: NextApiResponse;
-}>({ schema });
+} > ({
+  schema,
+  graphqlEndpoint: '/api/graphql',
+  plugins: [useResponseCache(
+      { // global cache
+        session: () => null,
+        ttl: 2 * 60 * 60 * 1000,
+        // ^ 2h * 60 mins per hour, 60 seconds per min 1000 miliseconds per second
+      }
+    )]
+});
 
 export default server;
