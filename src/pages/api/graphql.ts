31bfb357b3101d52bfeb7dcd0928c9d24bef6e18
NunoSempere <nuno.sempere@protonmail.com>
2022-11-09 21:36:19 +0000

    fix: continue yoga-graphql update

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 // apollo-server-micro is problematic since v3, see https://github.com/apollographql/apollo-server/issues/5547, so we use graphql-yoga instead
-import { createServer } from "@graphql-yoga/node";
+import { createYoga } from "@graphql-yoga";
 
 import { schema } from "../../graphql/schema";
 
-const server = createServer<{
+const server = createYoga<{
   req: NextApiRequest;
   res: NextApiResponse;
 }>({ schema });
