8ccb88558fb4d8dca7d3b2c2d96c7ed2d9c5b899
NunoSempere <nuno.sempere@protonmail.com>
2022-11-09 21:37:32 +0000

    fix: yoga import bug

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 // apollo-server-micro is problematic since v3, see https://github.com/apollographql/apollo-server/issues/5547, so we use graphql-yoga instead
-import { createYoga } from "@graphql-yoga";
+import { createYoga } from "graphql-yoga";
 
 import { schema } from "../../graphql/schema";
 
